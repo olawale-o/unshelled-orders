c69306c26032abc3e8cbea8878d729b930657148
olawale-o <omoogunolawale@gmail.com>
2023-02-04 09:20:09 +0100

    Create updateAccountService

--- src/services/accountService.js
+++ src/services/accountService.js
@@ -1,8 +1,17 @@
 import { api } from "../api";
 
 export const loginService = async (email, password) => {
-  const response = await api.post("/login", { email, password },  {
+  const response = await api.post("/account", { email, password },  {
     headers: { 'Content-Type': 'application/json' },
+    withCredentials: true
+  });
+  return response.data;
+};
+
+export const updateAccountService = async (data) => {
+  const response = await api.put('/account', data, {
+    headers: { 'Content-Type': 'application/json' },
+    withCredentials: true,
   });
   return response.data;
 };
